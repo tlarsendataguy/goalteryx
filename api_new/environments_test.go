4cabe04df6a8f4e9ea3d51a1c46e3537ef36633c
Tom <larsenthomasj@gmail.com>
2020-12-13 19:44:30 -0500

    clarification comment for environments_test

--- api_new/environments_test.go
+++ api_new/environments_test.go
@@ -4,8 +4,12 @@ import (
 	"testing"
 )
 
+// These unit tests require Alteryx to be installed.  Change the version below with your version of ALteryx.
+
+const version = `2020.2`
+
 func TestGetSettings(t *testing.T) {
-	locale := getLocale(`2020.2`)
+	locale := getLocale(version)
 	if locale != `en` {
 		t.Fatalf(`expected 'en' but got '%v'`, locale)
 	}
