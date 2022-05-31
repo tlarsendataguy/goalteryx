885373aff0af0d95dde81b8f8420e5f54510a436
tlarsen <larsenthomasj@gmail.com>
2022-05-31 06:10:41 -0400

    test no-cache output

--- sdk/sdk_nocache_test.go
+++ sdk/sdk_nocache_test.go
@@ -7,16 +7,26 @@ import (
 
 type NoCache struct {
 	RecordPackets int
+	output        sdk.OutputAnchor
+	info          *sdk.OutgoingRecordInfo
 }
 
 func (n *NoCache) Init(provider sdk.Provider) {
+	n.output = provider.GetOutputAnchor(`Output`)
 }
 
 func (n *NoCache) OnInputConnectionOpened(connection sdk.InputConnection) {
+	n.info = connection.Metadata().Clone().GenerateOutgoingRecordInfo()
+	n.output.Open(n.info)
 }
 
 func (n *NoCache) OnRecordPacket(connection sdk.InputConnection) {
 	n.RecordPackets++
+	packet := connection.Read()
+	for packet.Next() {
+		n.info.CopyFrom(packet.Record())
+		n.output.Write()
+	}
 }
 
 func (n *NoCache) OnComplete() {
@@ -26,9 +36,19 @@ func TestNoCache(t *testing.T) {
 	plugin := &NoCache{}
 	runner := sdk.RegisterToolTest(plugin, 1, ``, sdk.NoCache(true))
 	runner.ConnectInput(`Input`, `sdk_test_passthrough_simulation.txt`)
+	collector := runner.CaptureOutgoingAnchor(`Output`)
 	runner.SimulateLifecycle()
 
 	if plugin.RecordPackets != 4 {
 		t.Fatalf(`expected 4 packets but got %v`, plugin.RecordPackets)
+	}
+	if collector.PacketsReceived != 4 {
+		t.Fatalf(`expected 4 packets but got %v`, collector.PacketsReceived)
+	}
+	if fields := len(collector.Data); fields != 16 {
+		t.Fatalf(`expected 16 fields but got %v`, fields)
+	}
+	if records := len(collector.Data[`Field1`]); records != 4 {
+		t.Fatalf(`expected 4 records but got %v`, records)
 	}
 }
